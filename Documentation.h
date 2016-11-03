9b7ccb41c2e7fd5ed37692dd2411528ab2701812
johnkapaun <johnkapaun@comcast.net>
2016-11-03 16:43:35 -0500

    This sets the format for Doxygen (Documentation)

---
+++ Documentation.h
@@ -0,0 +1,60 @@
+/// @addtogroup Main
+/// @{
+		/// @addtogroup CINRun
+/// @}
+
+/// @addtogroup ProcessChannelData
+/// @{
+		/// @addtogroup GetChannelData
+		/// @addtogroup GetPaceData
+		/// @addtogroup GetRF_Data
+/// @}
+
+/// @addtogroup Report
+/// @{
+		/// @addtogroup PaceReport
+		/// @addtogroup RF_Report
+/// @}
+
+/// @addtogroup Filter
+/// @{
+		/// @addtogroup LowPass_Filter
+		/// @addtogroup iir_filter
+		/// @addtogroup prewarp
+		/// @addtogroup bilinear
+		/// @addtogroup szxform
+/// @}
+
+
+/// @addtogroup File
+/// @{
+		/// @addtogroup LoadBinFile
+		/// @addtogroup GetHeaderInfo
+/// @}
+
+/// @addtogroup Utility
+/// @{
+		/// @addtogroup ConvertNumber
+/// @}
+
+/// @mainpage RF_RectificationRead CIN Design Document
+/// <CENTER><B>"Version 1.0.0.0"</B></CENTER>\n \image html LV_RF_Rectification.JPG
+///
+/// @ref Main
+///
+/// @ref ProcessChannelData
+///
+/// @ref Report
+///
+/// @ref Filter
+///
+/// @ref File
+///
+/// @ref Utility
+/// 
+/// \n
+/// \n
+
+
+
+
